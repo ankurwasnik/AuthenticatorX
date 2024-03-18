3c624aff93cddb4ea54b7a5c3ad2916fb977d260
Ankur Wasnik <Ankur.Wasnik@rblbank.com>
2024-03-18 15:18:59 +0530

    enahanceAboutModule: added onClick to about list items

--- app/src/main/java/com/ankurwasnik/arcosauthenticator/model/support/SupportListItemModel.kt
+++ app/src/main/java/com/ankurwasnik/arcosauthenticator/model/support/SupportListItemModel.kt
@@ -1,6 +1,9 @@
 package com.ankurwasnik.arcosauthenticator.model.support
 
+import android.content.Context
+
 data class SupportListItemModel(
     val name :String,
-    val desc :String
+    val desc :String,
+    val onClick : (Context)->Unit = {}
 )
